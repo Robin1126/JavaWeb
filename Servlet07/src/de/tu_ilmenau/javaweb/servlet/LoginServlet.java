87f137faf4a215e1f547848f7bddd0d3a04eb290
Binbin Luo <luobinb7@gmail.com>
2023-03-23 19:33:58 +0100

    doPost method create and practice

---
+++ Servlet07/src/de/tu_ilmenau/javaweb/servlet/LoginServlet.java
@@ -0,0 +1,23 @@
+package de.tu_ilmenau.javaweb.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Author : Binbin Luo
+ * Date : 23.03.2023
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+       out.print("<h1>post请求已收到！</h1>");
+    }
+}
